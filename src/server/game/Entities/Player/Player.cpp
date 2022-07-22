384dd6882230f3160bedff9376ba1814bca7e8d1
itswicky <blaindes51@gmail.com>
2022-07-21 21:18:35 -0500

    Revert "Core/Player: Allow player to gain multiple ability points if gaining multiple levels"
    This reverts commit 862f4aefd48bd66b551e8ca07ff538187fb5c27d.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2661,10 +2661,7 @@ void Player::GiveLevel(uint8 level)
 
     if (level > oldLevel) // Done to not result in int overflow for neg number when losing levels i.e. gm command
         if (level % 2 == 0)
-        {
-            uint8 apCount = (oldLevel - level) / 2;
-            AddItem(60000, apCount); // Add custom Ability Point Token on level up
-        }
+            AddItem(60000, 1); // Add custom Ability Point Token on level up
 
 }
 
