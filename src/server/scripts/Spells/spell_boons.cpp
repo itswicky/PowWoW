edc756a0f2a16c8511b0403ed4670feaa6b06242
itswicky <blaindes51@gmail.com>
2023-08-02 02:39:46 -0500

    Core/Scripts: Add cpps files for affliction and boon spellscripts

---
+++ src/server/scripts/Spells/spell_boons.cpp
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /*
+  * Scripts for spells with SPELLFAMILY_GENERIC which cannot be included in AI script file
+  * of creature using it or can't be bound to any player class.
+  * Ordered alphabetically using scriptname.
+  * Scriptnames of files in this file should be prefixed with "spell_gen_"
+  */
+
+#include "ScriptMgr.h"
+#include "DBCStores.h"
+#include "GameTime.h"
+#include "GridNotifiersImpl.h"
+#include "Group.h"
+#include "InstanceScript.h"
+#include "Item.h"
+#include "Log.h"
+#include "MotionMaster.h"
+#include "ObjectMgr.h"
+#include "Pet.h"
+#include "SpellAuraEffects.h"
+#include "SpellHistory.h"
+#include "SpellMgr.h"
+#include "SpellScript.h"
+
+
+
+void AddSC_boons_spell_scripts()
+{
+
+}
--- src/server/scripts/Spells/spell_script_loader.cpp
+++ src/server/scripts/Spells/spell_script_loader.cpp
@@ -16,6 +16,8 @@
  */
 
 // This is where scripts' loading functions should be declared:
+void AddSC_affliction_spell_scripts();
+void AddSC_boons_spell_scripts();
 void AddSC_deathknight_spell_scripts();
 void AddSC_druid_spell_scripts();
 void AddSC_generic_spell_scripts();
@@ -34,6 +36,8 @@ void AddSC_item_spell_scripts();
 // void Add${NameOfDirectory}Scripts()
 void AddSpellsScripts()
 {
+    AddSC_affliction_spell_scripts();
+    AddSC_boons_spell_scripts();
     AddSC_deathknight_spell_scripts();
     AddSC_druid_spell_scripts();
     AddSC_generic_spell_scripts();
