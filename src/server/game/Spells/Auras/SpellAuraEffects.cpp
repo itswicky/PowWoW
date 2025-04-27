b4b8bcdfd0be5eff5dc7c65521797e7d24f667a4
itswicky <blaindes51@gmail.com>
2025-04-27 00:34:05 -0500

    Core/Spell: Add transform id for Ascension Lightning and lower min player GCD

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2070,6 +2070,9 @@ void AuraEffect::HandleAuraTransform(AuraApplication const* aurApp, uint8 mode, 
                     // Ascension: Fire
                     case 91360:
                         target->SetDisplayId(32762);
+                    // Ascension: Lightning
+                    case 91364:
+                        target->SetDisplayId(32760);
                     default:
                         break;
                 }
